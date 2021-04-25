1df7eafc900de69af3ae9dc53c7cd97c62b63131
Aaron Crosbie <aaroncrosie@outlook.com>
2021-04-25 13:09:16 +0100

    small improvements

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,17 +1,20 @@
 public class FizzBuzz {
     public static void main(String args[]) {
-        int fizz = 5;
-        int buzz = 3;
+        // int fizz = 5;
+        // int buzz = 3;
         for (int i = 0; i < 101; i++) {
-            if (i % (fizz * buzz) == 0) {
-                System.out.println("fizzBuzz");
-            } else if (i % buzz == 0) {
-                System.out.println("Buzz");
-            } else if (i % fizz == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
+            String output = "";
+            if (i % 5 == 0) {
+                output = output + "fizz";
             }
-        }
+            if (i % 3 == 0) {
+                output = output + "buzz";
+            }
+            if (output == "") {
+                output = String.valueOf(i);
+            }
+            System.out.println(output);
+
+            
     }
 }
